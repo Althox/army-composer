11a761b68263f45d18515af95e57de129d4d23c4
Althox <9414494+Althox@users.noreply.github.com>
2022-12-28 13:54:22 +0100

    updated root view
    enabled links and moved css to correct path

--- src/Controller/InfoController.php
+++ src/Controller/InfoController.php
@@ -2,13 +2,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-// use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class InfoController extends AbstractController
 {
-    #[Route('info/', name: 'about')]
+    #[Route('info/', name: 'info')]
     public function index()
     {
         echo phpinfo();
